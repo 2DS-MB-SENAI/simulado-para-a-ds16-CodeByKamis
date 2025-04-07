28c476262f9457d7d2961c73e794c28661159da9
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-04-07 10:43:29 -0300

    teste 7

--- livros/views.py
+++ livros/views.py
@@ -10,17 +10,17 @@ def listar_livros(request):
     livraria = Livro.objects.all()
     return render(request, 'livros.html', {'livraria':livraria})
 
-@api_view(['GET'])
-def read_livros(request):
-    livros = Livro.objects.all()
-    serializer = LivroSerializer(livros, many=True)
-    return Response(serializer.data)
 
-@api_view(['POST'])
-def creat_livros(request):
+@api_view(['POST', 'GET'])
+def teste(request):
     if request.method == 'POST':
         serializer= LivroSerializer(data=request.data, many=isinstance(request.data, list))
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        serializer = LivroSerializer(livros, many=True)
+        return Response(serializer.data)
+        
