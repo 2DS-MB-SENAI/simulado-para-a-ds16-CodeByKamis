3d5167bb9d7d0bb059af16d9763cf35929dce4f0
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-04-07 10:05:32 -0300

    teste 4

--- livros/views.py
+++ livros/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Livro
 def listar_livros(request):
     livraria = Livro.objects.all()
-    return render(request, '/livros.html', {'livraria':livraria})
+    return render(request, 'livros.html', {'livraria':livraria})
 
 # Create your views here.
