72992998803ec5d1e4b6244c108477ef0e4d0b57
Kamila Vit�ria de Souza Cardozo <56631494818@>
2025-04-07 10:32:17 -0300

    teste 6

--- livros/urls.py
+++ livros/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns =[
     path('livros/',views.listar_livros, name='listar_livros'),
+    path('livros/read/', views.read_livros, name='read_livros'),
+    path('livros/criar/', views.creat_livros, name='creat_livros'),
 ]
